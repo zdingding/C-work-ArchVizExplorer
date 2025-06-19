d2a3f53cc391225ed2d5d05d92292ba603e5d489
zdingding <zd_jobing@163.com>
2025-06-19 16:31:42 +0800

    C++

---
+++ Source/ArchVizExplorer/ArchVizExplorer.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ArchVizExplorer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ArchVizExplorer, "ArchVizExplorer" );
