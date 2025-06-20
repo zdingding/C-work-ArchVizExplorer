633fbaf5680359bca0c3e1880f8d90a7100c87f8
zdingding <zd_jobing@163.com>
2025-06-20 11:39:13 +0800

    一些插件

---
+++ Plugins/DigitalTwinFramework/Source/DigitalTwinFramework/Public/DigitalTwinBase.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "DigitalTwinBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract,Blueprintable)
+class DIGITALTWINFRAMEWORK_API UDigitalTwinBase : public UObject
+{
+	GENERATED_BODY()
+	// 初始化数字孪生系统
+UFUNCTION(BlueprintCallable, Category = "DigitalTwin")
+	virtual void InitializeDigitalTwin();
+	
+	// 获取所有场景
+	UFUNCTION(BlueprintCallable, Category = "DigitalTwin")
+	virtual TArray<FString> GetAvailableScenes() const;
+	
+	// 进入指定场景
+	UFUNCTION(BlueprintCallable, Category = "DigitalTwin")
+	virtual void EnterScene(const FString& SceneName);
+};
--- Source/ArchVizExplorer.Target.cs
+++ Source/ArchVizExplorer.Target.cs
@@ -9,7 +9,7 @@ public class ArchVizExplorerTarget : TargetRules
 	{
 		Type = TargetType.Game;
 		DefaultBuildSettings = BuildSettingsVersion.V4;
-
+	
 		ExtraModuleNames.AddRange( new string[] { "ArchVizExplorer" } );
 	}
 }
