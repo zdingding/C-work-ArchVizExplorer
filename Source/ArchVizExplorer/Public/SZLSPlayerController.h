e0137ec3246099cf385da02e739365a719df0cf8
zdingding <zd_jobing@163.com>
2025-07-01 15:02:57 +0800

    tq

---
+++ Source/ArchVizExplorer/Public/SZLSPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SZLSPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCHVIZEXPLORER_API ASZLSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
